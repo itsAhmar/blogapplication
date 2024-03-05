91622b5d3f3523d47d586030fc9a6478ac73fcb1
itsAhmar <bsef19a528@pucit.edu.pk>
2024-03-05 12:11:38 +0500

    Add read more button

--- app/javascript/controllers/read-more_controller.js
+++ app/javascript/controllers/read-more_controller.js
@@ -1,10 +1,20 @@
 import { Controller } from "@hotwired/stimulus";
 
 export default class extends Controller {
-  static targets = ["description"];
-  
+  static targets = ["description", "toggleButton"];
+  static values = { truncatedContent: String };
+
+  connect() {
+    this.truncatedContentValue = this.descriptionTarget.textContent.trim();
+  }
+
   toggle() {
- 		  this.descriptionTarget.textContent = this.data.get("my-value");
- 		  // console.log()
-	};
+    if (this.descriptionTarget.textContent.trim() === this.truncatedContentValue) {
+      this.descriptionTarget.textContent = this.data.get("my-value");
+      this.toggleButtonTarget.textContent = "Read Less";
+    } else {
+      this.descriptionTarget.textContent = this.truncatedContentValue;
+      this.toggleButtonTarget.textContent = "Read More";
+    }
+  }
 }
