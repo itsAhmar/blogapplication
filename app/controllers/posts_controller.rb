778eebbd83506961f50ec81d4d80fb181f4e8a8a
itsAhmar <bsef19a528@pucit.edu.pk>
2024-03-14 19:18:13 +0500

    Posts pagination using kaminari

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -3,7 +3,7 @@ class PostsController < ApplicationController
   before_action :set_post, only: %i[edit update destroy]
 
   def index
-    @posts = Post.order(id: :desc).includes(:user)
+    @posts = Post.order(id: :desc).includes(:user).page(params[:page])
   end
 
   def show
