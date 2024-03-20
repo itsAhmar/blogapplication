613bd640c17d8b27192199e0fb450b7bf7c94bb2
Haseeb Khan <haseebkhan.dev@gmail.com>
2024-03-20 16:16:37 +0500

    User model rspecs

--- spec/factories/posts.rb
+++ spec/factories/posts.rb
@@ -1,8 +1,7 @@
 FactoryBot.define do
   factory :post do
     title { Faker::Lorem.sentence }
-    description {Faker::Lorem.paragraph}
+    description { Faker::Lorem.paragraph }
     image { Rack::Test::UploadedFile.new(Rails.root.join('spec', 'fixtures', 'img.jpg'), 'image/jpeg') }
-    user
   end
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -18,26 +18,9 @@ RSpec.describe User, type: :model do
     it 'is valid' do
       expect(user).to be_valid
     end
-
-    # it 'has an attached image' do
-    #   expect(user.image).to be_attached
-    # end
-
-    # it 'validates presence of username' do
-    #   expect(user.username).to be_valid
-    # end
-
-    # it 'validates presence of email' do
-    #   expect(user).to validate_presence_of(:email)
-    # end
-
-    # it 'validates presence of password' do
-    #   expect(user).to validate_presence_of(:password)
-    # end
   end
 
   context 'when creating a user with invalid attributes' do
-
     it 'is invalid without any attributes' do
       user = User.new
       expect(user).to_not be_valid
@@ -61,6 +44,12 @@ RSpec.describe User, type: :model do
       expect(user).to_not be_valid
       expect(user.errors[:image]).to include("can't be blank")
     end
+
+    it 'when password is missing' do
+      user = build(:user, password: nil)
+      expect(user).to_not be_valid
+      expect(user.errors[:password]).to include("can't be blank")
+    end
   end
 
   context 'when testing user associations' do
@@ -69,7 +58,7 @@ RSpec.describe User, type: :model do
       expect(User.reflect_on_association(:posts).macro).to eq(:has_many)
     end
 
-     it 'has many comments' do
+    it 'has many comments' do
       expect(User.reflect_on_association(:comments).macro).to eq(:has_many)
     end
 
@@ -79,56 +68,21 @@ RSpec.describe User, type: :model do
   end
 
   context 'with dependent destroy associations' do
+    let(:user) { create :user }
+    let(:post) { create :post, user: }
+    let(:comment) { create :comment, user:, post: }
+
+    it 'destroys associated posts when user is deleted' do
+      expect { user.destroy }.to change { Post.count }.by(- user.posts.count)
+    end
+
+    it 'destroys associated comments when user is deleted' do
+      expect { user.destroy }.to change { Comment.count }.by(- user.comments.count)
+    end
+
+    it 'destroys associated like of type post when user is deleted' do
+      create(:like, user:, likeable: post)
+      expect { user.destroy }.to change { Like.count }.by(- user.likes.count)
+    end
   end
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    # it 'requires a password on creation' do
-    #   user2 = build(:user, password: nil) # Create a user without a password
-    #   expect(user2.valid?).to eq false # Ensure the user is not valid without a password
-    # end
-
-    # it 'requires a password on creation' do
-    #   user2 = build(:user) # Create a user without a password
-    #   expect(user2.valid?).to eq true # Ensure the user is not valid without a password
-    # end
-
-    # it 'requires a password on creation' do
-    #   user2 = build(:user, password: nil) # Create a user without a password
-    #   expect(user2.valid?).to eq false # Ensure the user is not valid without a password
-    #   expect(user2.errors[:password]).to include("can't be blank") # Ensure there is an error message for password presence
-    # end
-
-    # it 'requires a password with minimum 6 characters on creation' do
-    #   user2 = build(:user, password: '12345') # Create a user with a short password
-    #   expect(user2.valid?).to eq false # Ensure the user is not valid with a short password
-    #   expect(user2.errors[:password]).to include("is too short (minimum is 6 characters)") # Ensure there is an error message for password length
-    # end
-
-     # Negative test cases
-    # it 'should not have any other associations' do
-    #   unexpected_associations = User.reflect_on_all_associations.map(&:name) - [:posts, :comments, :likes]
-    #   expect(unexpected_associations).to be_empty
-    # end
-
-  
 end
-
-
-
